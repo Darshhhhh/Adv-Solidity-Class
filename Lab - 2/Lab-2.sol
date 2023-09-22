3b837caf2929a2b653513f03b4cca6d0d4634cde
Darshhhhh <contact.shahdarsh@gmail.com>
2023-09-22 14:05:34 -0400

    Lab - 2 Added

---
+++ Lab - 2/Lab-2.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract B {
+    uint256 public num;
+    address public sender;
+    uint256 public value;
+
+    function setVars(uint256 _num) public payable {
+        num = _num;
+        sender = msg.sender;
+        value = msg.value;
+    }
+}
+
+contract A {
+    uint256 public num;
+    address public sender;
+    uint256 public value;
+
+    function setVars(address _contract, uint256 _num) public payable {
+        // A's storage is set, B is not modified.
+        (bool success, bytes memory data) = _contract.delegatecall(
+            abi.encodeWithSelector(B.setVars.selector, _num)
+        );
+        require(success, "DelegateCall Failed!");
+    }
+}
